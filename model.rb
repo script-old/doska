a5c76e456045f7c49494a42b029263d206f42dff
Old Script <script.old@mail.ru>
2015-12-09 19:19:36 +0300

    изменил настройки подключения к базе

--- model.rb
+++ model.rb
@@ -1,6 +1,9 @@
 require 'mongo_mapper'
 require 'mongo_mapper_tree'
 #require 'bson_ext'
-$db = "doska"
+#$db = "doska"
+MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+MongoMapper.database = "doska"
+
 
 Dir["./models/*.rb"].each {|file| require file }
