3f3804ebaa91898cf5f0327d8fc17a13bd715501
Old Script <script.old@mail.ru>
2015-12-12 23:56:22 +0300

    Logger added

--- model.rb
+++ model.rb
@@ -3,7 +3,7 @@ require 'mongo_mapper_tree'
 require 'stringex'
 #require 'bson_ext'
 #$db = "doska"
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+MongoMapper.connection = Mongo::Connection.new('localhost', 27017, :logger => Logger.new(STDOUT))
 MongoMapper.database = "doska"
 
 
