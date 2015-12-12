e6af0880d9d27113304c5d8191cf2834671b4022
Old Script <script.old@mail.ru>
2015-12-12 15:27:06 +0300

    Редактирую модель пользователя Добавлен метод генерации уникального идентификатора Добавлена заглушка отправки сообщения новому пользователю

--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,10 @@
+require 'securerandom'
 class User
   include MongoMapper::Document
+  before_save :generate_uniq_url
+  after_save :send_new_email  #Необходимо заменить и сделать логику на случай активации по телефону
   key :first_name, String
-  key :las_name, String
+  key :last_name, String
   key :email, String
   key :bio, String
   key :password, String
@@ -12,4 +15,12 @@ class User
   
   many :adverts
   many :user_views
+  
+  private
+  def generate_uniq_url
+    self.activate_url = SecureRandom.uuid + "-" + SecureRandom.hex(4)
+  end
+  def send_new_email
+    puts "User #{first_name} created" #Заглушка. 
+  end
 end
