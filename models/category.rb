2df28d97df409c8251728b588f47f96a6bceddf6
Old Script <script.old@mail.ru>
2015-12-12 21:44:45 +0300

    В модели категории перед сохранением title преобразуется в slug После сохранения создается full_slug, потому что до сохранения невозможно получить "родителей"

--- models/category.rb
+++ models/category.rb
@@ -1,10 +1,23 @@
 class Category
   include MongoMapper::Document
   plugin  MongoMapper::Plugins::Tree #:name => "Child 1", :parent => @root_1, :value => 1
-  
+  before_save :make_cat_slug #different name from page method
+  after_save :make_full_slug #только после сохранения можно получить всех "родителей" и их данные. 
   key :title, String
-  key :slig, String
+  key :slug, String
+  key :full_slug, String
   key :description, String
   
   many :adverts
+  private
+  def make_cat_slug    
+    self.slug ||= title.to_url    
+  end
+  def make_full_slug
+    if self.root?
+      self.full_slug = self.slug
+    else
+      self.full_slug = self.ancestors.map(&:slug).join("/") + "/" + self.slug 
+    end
+  end
 end
