7fa0e7b067b39e68b8a3a89c009ba00f9b3e03e9
Old Script <script.old@mail.ru>
2015-12-13 15:03:11 +0300

    working

--- models/user.rb
+++ models/user.rb
@@ -5,13 +5,15 @@ class User
   after_save :send_new_email  #Необходимо заменить и сделать логику на случай активации по телефону
   key :first_name, String
   key :last_name, String
-  key :email, String
+  key :email, String, :unique => true
+  key :phone, Integer, :format => /(7|8)9\d{9}/
   key :bio, String
   key :password, String
   key :active, Boolean
-  key :created, Time
   key :last_visit, Time
-  key :activate_url, String
+  key :activate_url, String, :unique => true
+  
+  timestamps!
   
   many :adverts
   many :user_views
